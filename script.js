21060dacb85dea27014efed1a25dcaab7a0d147f
SAMTGS <161798093+SAMTGS@users.noreply.github.com>
2024-07-13 09:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,192 +1,75 @@
-        b1btn.disabled = true; 
-        b2btn.disabled = true; 
-        b4btn.disabled = true; 
-        b6btn.disabled = true; 
-        b8btn.disabled = true; 
-        b9btn.disabled = true; 
-  
-        b3btn.style.color = "red"; 
-        b5btn.style.color = "red"; 
-        b7btn.style.color = "red"; 
-    } 
-    else if ((b2 == '0' || b2 == '0') && (b5 == '0' || 
-        b5 == '0') && (b8 == '0' || b8 == '0')) { 
-        document.getElementById('print') 
-            .innerHTML = "Player 0 won"; 
-        b1btn.disabled = true; 
-        b3btn.disabled = true; 
-        b4btn.disabled = true; 
-        b6btn.disabled = true; 
-        b7btn.disabled = true; 
-        b9btn.disabled = true; 
-  
-        b2btn.style.color = "red"; 
-        b5btn.style.color = "red"; 
-        b8btn.style.color = "red"; 
-    } 
-    else if ((b4 == '0' || b4 == '0') && (b5 == '0' || 
-        b5 == '0') && (b6 == '0' || b6 == '0')) { 
-        document.getElementById('print') 
-            .innerHTML = "Player 0 won"; 
-        b1btn.disabled = true; 
-        b2btn.disabled = true; 
-        b3btn.disabled = true; 
-        b7btn.disabled = true; 
-        b8btn.disabled = true; 
-        b9btn.disabled = true; 
-  
-        b4btn.style.color = "red"; 
-        b5btn.style.color = "red"; 
-        b6btn.style.color = "red"; 
-    } 
-  
-    // Checking of Player 0 finish 
-    // Here, Checking about Tie 
-    else if ((b1 == 'X' || b1 == '0') && (b2 == 'X'
-        || b2 == '0') && (b3 == 'X' || b3 == '0') && 
-        (b4 == 'X' || b4 == '0') && (b5 == 'X' || 
-            b5 == '0') && (b6 == 'X' || b6 == '0') && 
-        (b7 == 'X' || b7 == '0') && (b8 == 'X' || 
-            b8 == '0') && (b9 == 'X' || b9 == '0')) { 
-        document.getElementById('print') 
-            .innerHTML = "Match Tie"; 
-    } 
-    else { 
-  
-        // Here, Printing Result 
-        if (flag == 1) { 
-            document.getElementById('print') 
-                .innerHTML = "Player X Turn"; 
-        } 
-        else { 
-            document.getElementById('print') 
-                .innerHTML = "Player 0 Turn"; 
-        } 
-    } 
-} 
-  
-// Function to reset game 
-function myfunc_2() { 
-    location.reload(); 
-    b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = ''; 
-} 
-  
-// Here onwards, functions check turn of the player 
-// and put accordingly value X or 0 
-flag = 1; 
-function myfunc_3() { 
-    if (flag == 1) { 
-        document.getElementById("b1").value = "X"; 
-        document.getElementById("b1").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b1").value = "0"; 
-        document.getElementById("b1").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_4() { 
-    if (flag == 1) { 
-        document.getElementById("b2").value = "X"; 
-        document.getElementById("b2").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b2").value = "0"; 
-        document.getElementById("b2").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_5() { 
-    if (flag == 1) { 
-        document.getElementById("b3").value = "X"; 
-        document.getElementById("b3").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b3").value = "0"; 
-        document.getElementById("b3").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_6() { 
-    if (flag == 1) { 
-        document.getElementById("b4").value = "X"; 
-        document.getElementById("b4").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b4").value = "0"; 
-        document.getElementById("b4").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_7() { 
-    if (flag == 1) { 
-        document.getElementById("b5").value = "X"; 
-        document.getElementById("b5").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b5").value = "0"; 
-        document.getElementById("b5").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_8() { 
-    if (flag == 1) { 
-        document.getElementById("b6").value = "X"; 
-        document.getElementById("b6").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b6").value = "0"; 
-        document.getElementById("b6").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_9() { 
-    if (flag == 1) { 
-        document.getElementById("b7").value = "X"; 
-        document.getElementById("b7").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b7").value = "0"; 
-        document.getElementById("b7").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_10() { 
-    if (flag == 1) { 
-        document.getElementById("b8").value = "X"; 
-        document.getElementById("b8").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b8").value = "0"; 
-        document.getElementById("b8").disabled = true; 
-        flag = 1; 
-    } 
-} 
-  
-function myfunc_11() { 
-    if (flag == 1) { 
-        document.getElementById("b9").value = "X"; 
-        document.getElementById("b9").disabled = true; 
-        flag = 0; 
-    } 
-    else { 
-        document.getElementById("b9").value = "0"; 
-        document.getElementById("b9").disabled = true; 
-        flag = 1; 
-    } 
-} 
+const board = document.getElementById('board')
+const squares = document.getElementsByClassName('square')
+const players = ['X', 'O']
+let currentPlayer = players[0]
+const endMessage = document.createElement('h2')
+endMessage.textContent = `X's turn!`
+endMessage.style.marginTop = '30px'
+endMessage.style.textAlign='center'
+board.after(endMessage)
+
+const winning_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+for(let i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', () => {
+        if(squares[i].textContent !== ''){
+            return
+        }
+        squares[i].textContent = currentPlayer
+        if(checkWin(currentPlayer)) {
+            endMessage.textContent=`Game over! ${currentPlayer} wins!`
+            return
+        }
+        if(checkTie()) {
+            endMessage.textContent= `Game is tied!`
+            return
+        }
+        currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0] 
+        if(currentPlayer == players[0]) {
+            endMessage.textContent= `X's turn!`
+        } else {
+            endMessage.textContent= `O's turn!`
+        }     
+    })   
+}
+
+function checkWin(currentPlayer) {
+    for(let i = 0; i < winning_combinations.length; i++){
+        const [a, b, c] = winning_combinations[i]
+        if(squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer){
+            return true
+        }
+    }
+    return false
+}
+
+function checkTie(){
+    for(let i = 0; i < squares.length; i++) {
+        if(squares[i].textContent === '') {
+            return false;
+        }
+    }
+    return true
+}
+
+function restartButton() {
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].textContent = ""
+    }
+    endMessage.textContent=`X's turn!`
+    currentPlayer = players[0]
+}
+
+
+
+
+
